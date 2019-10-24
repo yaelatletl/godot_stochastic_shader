914617c48ef0e1775ff935567724741cba94c7d4
yael <yael.atletl@gmail.com>
2019-10-24 02:21:45 -0500

    Discard computationally heavy alogrithm, replace with optimal

--- Stochastic.gd
+++ Stochastic.gd
@@ -1,10 +1,9 @@
 tool
 extends MeshInstance
 export(Texture) var albedo
-onready var Math = preload("res://bin/AdvancedMath.gdns").new()
 onready var MathLib = GDNative.new()
-const GAUSSIAN_AVERAGE = 1.0
-const GAUSSIAN_STD = 1.0
+const GAUSSIAN_AVERAGE = 0.5
+const GAUSSIAN_STD = 1.0/(6.0*6.0)
 signal Tinv_ready
 signal TInput_ready
 
@@ -18,13 +17,15 @@ func _ready():
 	if input == null:
 		input = albedo
 	var Tinput = Image.new()
-	Tinput.create(input.get_width(), input.get_height(), false, Image.FORMAT_RGB8)
+	Tinput.create(input.get_width(), input.get_height(), false, Image.FORMAT_RGBAF)
 	var Tinv = Image.new()
-	Tinv.create(input.get_width(), input.get_height(), false, Image.FORMAT_RGB8)
+	Tinv.create(input.get_width(), input.get_height(), false, Image.FORMAT_RGBAF)
 	input = input.get_data()
+	input.lock()
 	Tinv = ComputeinvT(input, Tinv)
-	Tinput = ComputeTinput(input, Tinput)
 	Tinv.save_png("res://tinv.png")
+	Tinput = ComputeTinput(input, Tinput)
+	
 	Tinput.save_png("res://tinput.png")
 	mat.set_shader_param("texture_albedo_tinv", Tinv)
 	mat.set_shader_param("texture_albedo_input", Tinput)
@@ -34,14 +35,14 @@ func _ready():
 	
 func Erf( input : float) -> float:
 #	var res : float  = MathLib.call_native("float", "erf", [input])
-	var res = Math.erf(input)
-	print(res)
+	var res = erf(input)
+	
 	return res
 	
 func ErfInv( input : float) -> float:
 #	var res : float  = MathLib.call_native("float", "inverf", [input])
-	var res = Math.inverf(input)
-	print(res)
+	var res = inverf(input)
+	
 	return res
 
 
@@ -52,53 +53,117 @@ func CDF ( x:float , mu:float , sigma:float ) -> float:
 func invCDF ( U : float , mu : float , sigma : float ) -> float:
 	var x : float = sigma * sqrt (2.0) * ErfInv (2.0*U -1.0) + mu 
 	return x
-	
+
+class Sorter:
+	static func sort(a, b):
+		if a.y < b.x:
+			return true
+		return false
+
 func ComputeTinput ( input : Image ,  T_input : Image )-> Image:
 	print("Enter Tinput computation")
+	T_input.lock()
 	# Sort pixels of example image
-	var sortedInputValues:Array ;
-	sortedInputValues.resize ( input . get_width() * input . get_height() );
+	var sortedInputValues: Array 
+	var OutputColors : PoolVector3Array
+	
+	sortedInputValues.resize ( input . get_width() * input . get_height() )
+	OutputColors.resize(sortedInputValues.size())
 	input.lock()
-	for y in range(0, input.height):
+	
+	for y in range(0, input.get_height()):
 		for x in range(0, input.get_width()):
-			sortedInputValues [ y * input . get_width() + x ]. x = x;
-			sortedInputValues [ y * input . get_width() + x ]. y = y;
-			sortedInputValues [ y * input . get_width() + x ]. value = input . get_pixel (x , y);
-
-	sortedInputValues.sort();
-
-	for i in range(0, sortedInputValues.size ()):
+				sortedInputValues [ y * input . get_width() + x ] =  Vector3(
+				x, 
+				y,
+				input . get_pixel (x , y).to_rgba32())
+	var sortedR = sortedInputValues.duplicate(true)
+	var sortedG = sortedInputValues.duplicate(true)
+	var sortedB = sortedInputValues.duplicate(true)
+	
+	for i in range(0,sortedInputValues.size()):
+		sortedR[i].z = Color(int(sortedR[i].z)).r
+		sortedG[i].z = Color(int(sortedG[i].z)).g
+		sortedB[i].z = Color(int(sortedB[i].z)).b
+		
+	sortedR.sort()#_custom(Sorter, "sort")
+	sortedG.sort()#_custom(Sorter, "sort")
+	sortedB.sort()
+	
+	for channels in range(0,2):
+		for i in range(0, sortedInputValues.size ()):
+			var x : int
+			var y : int
+			match channels:
+				0:
+					x = sortedR [i]. x;
+					y = sortedR [i]. y;
+				1:
+					x = sortedG [i]. x;
+					y = sortedG [i]. y;
+				2:
+					x = sortedB [i]. x;
+					y = sortedB [i]. y;
+	# Input quantile ( given by its order in the sorting )
+			var U : float = (i + 0.5 ) / ( sortedInputValues.size () );
+	# Gaussian quantile
+			var G : float= invCDF (U , GAUSSIAN_AVERAGE , GAUSSIAN_STD );
+	# Store
+			match channels:
+				0:
+					OutputColors[i].x = G
+				1:
+					OutputColors[i].y = G
+				2:
+					OutputColors[i].z = G
+	for i in range(0,OutputColors.size()):
 		var x : int = sortedInputValues [i]. x;
 		var y : int = sortedInputValues [i]. y;
-	# Input quantile ( given by its order in the sorting )
-		var U : float = (i + 0.5 ) / ( sortedInputValues.size () );
-	# Gaussian quantile
-		var G : float= invCDF (U , GAUSSIAN_AVERAGE , GAUSSIAN_STD );
-	# Store
-		T_input . set_pixel (x , y , Color(G))
+		T_input . set_pixel (x , y , Color(OutputColors[i].x,OutputColors[i].y, OutputColors[i].z))
 	emit_signal("TInput_ready")
 	return T_input
 	
 func ComputeinvT ( input : Image , Tinv : Image) -> Image:
 	print("Enter Tinv computation")
+	var output : Image = Image.new()
+	
 	# Sort pixels of example image
+	
 	var sortedInputValues : Array ;
-	sortedInputValues . resize ( input.get_width() * input .get_height() );
+	sortedInputValues . resize ( input.get_width() * input .get_height() )
+	output.create(16384,1,false,Image.FORMAT_RGBF)
+	output.lock()
 	input.lock()
 	for y in range( 0,  input . get_height()):
 		for x in range( 0,  input . get_width()):
-			sortedInputValues [y * input . get_width() + x] = input . get_pixel (x , y );
-	sortedInputValues.sort()
-	for i in range(0 ,  Tinv . get_width()):
+			sortedInputValues [y * input . get_width() + x] = input . get_pixel (x , y ).to_rgba32();
+	
+	var sortedR = sortedInputValues.duplicate(true)
+	var sortedG = sortedInputValues.duplicate(true)
+	var sortedB = sortedInputValues.duplicate(true)
+	
+	for i in range(0,sortedInputValues.size()):
+		sortedR[i] = Color(int(sortedInputValues[i])).r
+		sortedG[i] = Color(int(sortedInputValues[i])).g
+		sortedB[i] = Color(int(sortedInputValues[i])).b
+		
+	sortedR.sort()#_custom(Sorter, "sort")
+	sortedG.sort()#_custom(Sorter, "sort")
+	sortedB.sort()
+	for i in range(0 ,  output . get_width()):
 # Gaussian value in [0 , 1]
-		var G : float = (i + 0.5) / ( Tinv . get_width() );
+		var G : float = (i + 0.5) / ( output . get_width() );
 # Quantile value
 		var U : float = CDF (G , GAUSSIAN_AVERAGE , GAUSSIAN_STD ) ;
 # Find quantile in sorted pixel values
-		var index : int =  floor (U * sortedInputValues . size () );
+		var index : int =  floor (U * (sortedInputValues . size () -1));
 # Get input value
-		var I : float = sortedInputValues [ index ];
+		
+		var IR : float = sortedR[index]
+		var IG : float = sortedG[index]
+		var IB : float = sortedB[index]
+		
 # Store in LUT
-		Tinv . set_pixel (i , 0 , Color(I) );
+		output . set_pixel (i , 0 , Color(IR,IG,IB));
 	emit_signal("Tinv_ready")
-	return Tinv
+	return output
